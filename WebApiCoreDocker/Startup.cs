ebf560e50146a67d75dcad5fee72dafad6ec1c9c
Andrius V <andrius@live.se>
2019-11-05 10:32:17 +0100

    Added Swagger;

--- WebApiCoreDocker/Startup.cs
+++ WebApiCoreDocker/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApiCoreDocker
 {
@@ -26,6 +27,11 @@ namespace WebApiCoreDocker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "WebApiCoreDocker", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +47,14 @@ namespace WebApiCoreDocker
                 app.UseHsts();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiCoreDocker V1");
+            });
+
             app.UseHttpsRedirection();
+            
             app.UseMvc();
         }
     }
