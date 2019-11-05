fd06279baab222ea86f12bc1126341c3d6c38102
Andrius V <andrius@live.se>
2019-11-05 16:51:19 +0100

    Added Update and Delete; Renamed variables and refactored;

--- WebApiCoreDocker/Controllers/CustomersController.cs
+++ WebApiCoreDocker/Controllers/CustomersController.cs
@@ -23,24 +23,19 @@ namespace WebApiCoreDocker.Controllers
         // GET api/customers/1234567890
         [HttpGet("{securityNumber}")]
         public IActionResult Get(string securityNumber)
-        {
-
-            var response = new CustomerResponse();
-
+        {   
             if (!string.IsNullOrEmpty(securityNumber))
             {
-                var existing = _db.Customers.Find(securityNumber);
-                if (existing == null)
-                {
-                    response.ErrorMessage = "Customer does not exist.";
-                    return BadRequest(response);
+                var customer = _db.Customers.Find(securityNumber);
+                if (customer == null)
+                {                   
+                    return NotFound();
                 }
 
-                return Ok(existing);
+                return Ok(customer);
             }
 
-            return BadRequest(response);
-
+            return BadRequest();
         }
 
         // POST api/customers
@@ -51,8 +46,8 @@ namespace WebApiCoreDocker.Controllers
 
             if (!string.IsNullOrEmpty(value.SecurityNumber))
             {
-                var existing = _db.Customers.Find(value.SecurityNumber);
-                if (existing != null)
+                var customer = _db.Customers.Find(value.SecurityNumber);
+                if (customer != null)
                 {
                     response.ErrorMessage = "Customer already exists.";
                     return BadRequest(response);
@@ -69,10 +64,81 @@ namespace WebApiCoreDocker.Controllers
                 {
                     // Log message ex
                     response.ErrorMessage = "Error on Customer saving ...";
+
+                    return BadRequest(response);
                 }
             }
 
-            return BadRequest(response);
+            return BadRequest();
+        }
+
+        // PUT api/customers/1234567890
+        [HttpPut("{securityNumber}")]
+        public IActionResult Put(string securityNumber, [FromForm] Customer value)
+        {
+            if (!string.IsNullOrEmpty(securityNumber) && securityNumber == value.SecurityNumber)
+            {
+
+                var customer = _db.Customers.Find(securityNumber);
+
+                if (customer != null)
+                {
+                    try
+                    {
+                        customer.PhoneNumber = value.PhoneNumber;
+                        customer.EmailAddress = value.EmailAddress;
+                        _db.Update(customer);
+                        _db.SaveChanges();
+
+                        return Ok();
+                    }
+                    catch (Exception ex)
+                    {                        
+                        // Log message
+                        var response = new CustomerResponse();
+                        response.ErrorMessage = "Error on Customer updating ...";
+
+                        return BadRequest(response);
+                    }
+                }
+            }
+
+            return BadRequest();
+        }
+
+        // DELETE api/customers/1234567890
+        [HttpDelete("{securityNumber}")]
+        public IActionResult Delete(string securityNumber)
+        {
+            if (!string.IsNullOrEmpty(securityNumber))
+            {
+                var customer = _db.Customers.Find(securityNumber);
+
+                if (customer != null)
+                {
+                    try
+                    {
+                        _db.Remove(customer);
+                        _db.SaveChanges();
+
+                        return Ok();
+                    }
+                    catch (Exception ex)
+                    {
+                        // Log message                        
+                        var response = new CustomerResponse();
+                        response.ErrorMessage = "Error on Customer deleting ...";
+
+                        return BadRequest(response);
+                    }
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+            return BadRequest();
         }
     }
 }
