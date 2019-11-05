077e9c00faf692f5109e5c747b62a134a7e0a69e
Andrius V <andrius@live.se>
2019-11-05 13:41:31 +0100

    Added Customer class;

---
+++ WebApiCoreDocker/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCoreDocker.Models
+{
+    public class Customer
+    {
+        [Key]
+        public string SecurityNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
